66b5a5383d04282eb7d2622752a4496cbf9c3e95
Derek Anderson <Phlarx@users.noreply.github.com>
2017-06-21 16:47:17 -0500

    Add unicode support to chipword
    tools/chipword.py
        Decodes input as UTF8 so non-ASCII characters can be represented

--- tools/chipword.py
+++ tools/chipword.py
@@ -13,19 +13,16 @@ if len(argv) == 1:
 	exit(-1)
 
 word = ' '.join(argv[1:])
-
-def x(s,m):
-	t = [ord(c) & m for c in s]
-	return zip(t[:-1], t[1:])
+word = bytes(word, 'UTF8')
 
 p = "*Z~.\n,--'\n>" + ("Z" * (len(word)-1)) + "t\n"
-h = "".join([(")" if ord(x) & 0x80 else "x") for x in word]) + "h\n"
-g = "".join([(")" if ord(x) & 0x40 else "x") for x in word]) + "g\n"
-f = "".join([(")" if ord(x) & 0x20 else "x") for x in word]) + "f\n"
-e = "".join([(")" if ord(x) & 0x10 else "x") for x in word]) + "e\n"
-d = "".join([(")" if ord(x) & 0x08 else "x") for x in word]) + "d\n"
-c = "".join([(")" if ord(x) & 0x04 else "x") for x in word]) + "c\n"
-b = "".join([(")" if ord(x) & 0x02 else "x") for x in word]) + "b\n"
-a = "".join([(")" if ord(x) & 0x01 else "x") for x in word]) + "a"
+h = "".join([(")" if x & 0x80 else "x") for x in word]) + "h\n"
+g = "".join([(")" if x & 0x40 else "x") for x in word]) + "g\n"
+f = "".join([(")" if x & 0x20 else "x") for x in word]) + "f\n"
+e = "".join([(")" if x & 0x10 else "x") for x in word]) + "e\n"
+d = "".join([(")" if x & 0x08 else "x") for x in word]) + "d\n"
+c = "".join([(")" if x & 0x04 else "x") for x in word]) + "c\n"
+b = "".join([(")" if x & 0x02 else "x") for x in word]) + "b\n"
+a = "".join([(")" if x & 0x01 else "x") for x in word]) + "a"
 
 print(p + h + g + f + e + d + c + b + a)
